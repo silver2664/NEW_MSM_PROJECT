da2b592b2a31d31a2fb66f1c7555cf909fe43153
minjihong1 <dcba7516@naver.com>
2020-02-07 11:42:01 +0900

    ckeditor config.js 에디터 기본 높이 변경

--- MSM/src/main/webapp/resources/js/ckeditor/config.js
+++ MSM/src/main/webapp/resources/js/ckeditor/config.js
@@ -13,4 +13,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.height = '600px';		//Editor 높이 
 };
